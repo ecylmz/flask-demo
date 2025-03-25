65a5c9ac0ebd8c37c18067c85d8fa5eafaeb2652
sitiarel <emrecan@bordo.io>
2025-03-25 14:17:26 +0300

    değişiklik

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     name = os.environ.get("NAME", "Default")
-    return f"Hello, {name}. Welcome to the CI/CD World!"
+    return f"Hello, {name}. Welcome to the CI/CD World!1"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
